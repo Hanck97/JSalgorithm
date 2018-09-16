88b06de6f3aa2948160e63df7065425d21fc69f1
hanyang <hanyang@meili-inc.com>
2018-09-16 22:24:59 +0800

    引入Echarts组件样式

--- permutate.js
+++ permutate.js
@@ -12,8 +12,9 @@ function permutate(str) {
         var preResult = permutate(rest);
         for (var i = 0; i  < preResult.length ; i ++) {
             for (var j = 0; j < preResult.length + 1; j ++) {
-                var tmp = preResult[i].slice(0, j) + left + preResult[i].slice(j, preResult[i].length);            }
+                var tmp = preResult[i].slice(0, j) + left + preResult[i].slice(j, preResult[i].length);            
                 result.push(tmp);
+            }
         }
     } else if (str.length == 1) {
         return [str];
